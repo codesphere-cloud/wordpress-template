762fe877554c3cdf94e13f84d1c6e702d072812f
schrodit <mail@timschrodi.tech>
2022-08-11 09:39:13 +0200

    Add wordpress template and helper scripts

--- start.sh
+++ start.sh
@@ -3,4 +3,13 @@
 SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
+mkdir -p /tmp/log
+mkdir -p /tmp/run
+
+if [ ! -f "/tmp/run/php7.4-fpm.pid" ]; then
+    echo "Start PHP FPM Service"
+    php-fpm7.4 -y $SCRIPTPATH/php/php-fpm.conf
+fi
+
+echo "Start nginx on port 3000"
 nginx -c $SCRIPTPATH/nginx.conf
---
+++ wp-config.php
@@ -0,0 +1,1 @@
+./html/wp-config.php
