1058bafd563dc5471782bf444f8db44d8243669f
Tim Schrodi <mail@timschrodi.tech>
2023-03-06 12:29:59 +0100

    (#11) Improve php sigint handling, show logs in ci stage
    CU-332ghb8
    CU-8669kx75m

--- start.sh
+++ start.sh
@@ -10,10 +10,19 @@ mkdir -p /tmp/run
 
 export PHPRC=$SCRIPTPATH/php
 
-if [ ! -f "/tmp/run/php7.4-fpm.pid" ]; then
-    echo "Start PHP FPM Service"
-    php-fpm7.4 -y $SCRIPTPATH/php/php-fpm.conf
+function stop_php() {
+    kill "$(cat /tmp/run/php7.4-fpm.pid)"
+    rm /tmp/run/php7.4-fpm.pid
+}
+
+if [ -f "/tmp/run/php7.4-fpm.pid" ]; then
+    stop_php
 fi
 
+echo "Start PHP FPM Service"
+php-fpm7.4 -y $SCRIPTPATH/php/php-fpm.conf
+
+trap stop_php EXIT
+
 echo "Start nginx on port 3000"
-nginx -c $SCRIPTPATH/nginx.conf -g 'daemon off;'
+nginx -c $SCRIPTPATH/nginx/nginx.conf -g 'daemon off;'
--- stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SCRIPT=$(realpath "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-
-nginx -c $SCRIPTPATH/nginx.conf -s quit
